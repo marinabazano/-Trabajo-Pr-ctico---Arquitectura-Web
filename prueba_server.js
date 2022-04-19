d8289d60a592a1d3c70d1013daf3220c0c7e8a20
Marina Bazano Sammartino <mbazanosammartino@buenosaires.gob.ar>
2022-04-19 09:40:16 -0300

    Actualizacion de scripts de prueba

--- prueba_server.js
+++ prueba_server.js
@@ -1,18 +1,43 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const { redirect } = require('express/lib/response')
 const app = express()
 const port = 3000
 
 
+var requestTime = function (req, res, next) {
+    var todaysDate = new Date();
+    //req.requestTime = todaysDate.prototype.getDate();
+    req.requestTime = todaysDate;
+    next();
+};
+  
+app.use(requestTime);
+
 // http://localhost:3000/
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+    var responseText = 'Main Page';
+    responseText += ' Requested at: ' + req.requestTime + '';
+    res.send(responseText)
 })
 
-// http://localhost:3000/projects/
-app.get('/projects/', (req, res) => {
-    res.send('Acá se van a listar los proyectos')
+
+// Projects
+
+// http://localhost:3000/projects
+app.get('/projects', async (req, res) => {
+    res.send('Acá se van a listar todos los proyectos')
 })
+  
+// http://localhost:3000/projects/:idProject
+app.get('/projects/:idProject', async (req, res) => {
+    res.send('Acá se va a listar el proyecto con idProject')
+})
+
+// Tasks
+
+// Employees
   
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
